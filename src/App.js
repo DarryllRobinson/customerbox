ecbf9fef144c203239d258a67db2a2a6dadbe9a8
Darryll Robinson <darryll@stillproud.com>
2022-06-27 16:58:46 +1000

    Just getting things working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './assets/images/CustomerBox banner.png';
-import './App.css';
+import React from 'react';
+import { Navigate, Route, Router, Routes, useLocation } from 'react-router-dom';
+import { Box, Container } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import Home from './components/Home';
+import Copyright from './components/Copyright';
+import Dashboard from './components/Dashboard';
+
+// User pages
+import SignIn from './features/users/SignIn';
+import SignUp from './features/users/SignUp';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
 
 function App() {
+  let { pathname } = useLocation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          border: '1px solid black',
+          display: 'flex',
+          flexDirection: 'column',
+          m: '-8px',
+          minHeight: '100vh',
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
+        <Routes>
+          <Route
+            path="/:url*(/+)"
+            element={<Navigate to={pathname.slice(0, -1)} replace />}
+          />
+          <Route path="/" element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="signup" element={<SignUp />} />
+        </Routes>
+        <Box
+          component="footer"
+          sx={{
+            py: 1,
+            px: 2,
+            mt: 'auto',
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[300]
+                : theme.palette.grey[900],
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Container maxWidth="sm">
+            <Copyright sx={{ pt: 2 }} />
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export { App };
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Grid, Paper, Skeleton } from '@mui/material';
+
+import NavBar from './NavBar';
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+      Redeemt
+      <Grid></Grid>
+    </>
+  );
+}
+
+export default Home;
