4c59924bfa39a55e0ef509152725965505622022
Darryll Robinson <darryll@stillproud.com>
2022-07-21 22:02:27 +1000

    Quick save before working on some pricing work

--- src/features/nav/mobile/__test__/MenuChoices.test.js
+++ src/features/nav/mobile/__test__/MenuChoices.test.js
@@ -3,6 +3,24 @@ import { useTheme } from '@mui/material/styles';
 
 import MenuChoices from '../MenuChoices';
 
+jest.mock('useTheme', () => {
+  const Styles = jest.requireActual('@material-ui/core/styles');
+
+  const createMuiTheme = jest.requireActual(
+    '@material-ui/core/styles/createMuiTheme'
+  ).default;
+
+  const options = jest.requireActual('../../../src/themes/options').default;
+
+  return {
+    ...Styles,
+    makeStyles: (func) => {
+      const theme = createMuiTheme(options);
+      return Styles.makeStyles(func.bind(null, theme));
+    },
+  };
+});
+
 describe('Testing the MenuChoices component', () => {
   beforeEach(() => {
     render(<MenuChoices />);
